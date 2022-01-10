e28a8bf07e79300c12b570462372fe2ce5585051
HIdeyuki Inokuchi <hideyukiinokuchi@HIdeyukinoMacBook-Pro.local>
2022-01-10 19:39:02 +0900

    firebaseの構築

--- components/firebase.ts
+++ components/firebase.ts
@@ -2,14 +2,16 @@ import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 import "firebase/compat/firestore"
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_APILEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+ const firebaseApp = firebase.initializeApp({
+  apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:process.env.REACT_APP_FIREBASE_DOMAIN,
   projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket:process.env.REACT_APP_FIREBASE_PROJECT,
+  storageBucket:process.env.REACT_APP_FIREBASE_STOTAGE_BUCKET,
   messagingSenderId:process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId:process.env.REACT_APP_FIREBASE_APP_ID
 })
+
+console.log(firebaseApp)
 
 export const db = firebaseApp.firestore()
 export const auth = firebaseApp.auth()
--- pages/App.tsx
+++ pages/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { db } from "../components/firebase";
 
-const App:React.FC = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  useEffect(() => {
+    const unSub = db.collection("tasks").onSnapshot((snapshot) => {
+      setTasks(
+        snapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
+      );
+    });
+    return () => unSub();
+  }, []);
+  return (
+    <div>
+      {tasks.map((task,index) => (
+        <h3 key={index}>{task.title}</h3>
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
