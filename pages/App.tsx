f187c8f727fc8c1ac19ccc0ea1ea5c57e9e80bb2
HIdeyuki Inokuchi <hideyukiinokuchi@HIdeyukinoMacBook-Pro.local>
2022-01-10 23:14:04 +0900

    taskItemsコンポーネント作成

--- pages/App.tsx
+++ pages/App.tsx
@@ -1,8 +1,13 @@
+import { Button, FormControl, List, TextField } from "@mui/material";
+import AddBoxIcon from "@mui/icons-material/AddBox";
 import React, { useState, useEffect } from "react";
 import { db } from "../components/firebase";
+import TaskItems from "../components/TaskItems";
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [term, setTerm] = useState("");
+
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
       setTasks(
@@ -11,11 +16,41 @@ const App: React.FC = () => {
     });
     return () => unSub();
   }, []);
+
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    db.collection("tasks").add({ title: term });
+    setTerm("");
+  };
   return (
     <div>
-      {tasks.map((task,index) => (
-        <h3 key={index}>{task.title}</h3>
-      ))}
+      <h1 style={{ textAlign: "center" }}>todoリスト</h1>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <FormControl>
+          <TextField
+            label="NEW task?"
+            value={term}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setTerm(e.target.value);
+            }}
+            InputLabelProps={{ shrink: true }}
+          />
+        </FormControl>
+        <button disabled={!term} onClick={newTask}>
+          <AddBoxIcon />
+        </button>
+      </div>
+
+      <List>
+        {tasks.map((task) => (
+          <TaskItems key={task.id} id={task.id} title={task.title} />
+        ))}
+      </List>
     </div>
   );
 };
